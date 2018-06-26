03a7ba82a360204f97d6fedd0b5cbc0416bb42ee
icew4ll <icew4ll.io>
2018-06-26 08:14:18 -0700

    2018-06-26 15:14:18.532564301 UTC

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,45 @@
+// imports {{{
+// macros
 #[macro_use]
 extern crate dotenv_codegen;
 #[macro_use]
 extern crate duct;
 #[macro_use]
 extern crate lazy_static;
-// #[macro_use]
-// extern crate prettytable;
+// regular
+#[macro_use]
+extern crate prettytable;
 extern crate chrono;
 extern crate clap;
 extern crate maxminddb;
 extern crate regex;
-
+// usage
 use chrono::prelude::*;
 use clap::{App, Arg, SubCommand};
 use duct::cmd;
 use maxminddb::geoip2;
+use prettytable::Table;
 use regex::Regex;
 use std::env;
 use std::net::IpAddr;
 use std::str::FromStr;
-
+type MQ = (String, String, String);
+// use prettytable::row::Row;
+// use prettytable::cell::Cell;
+// }}}
+// vec of strings macro {{{
 // macro_rules! vec_of_strings {
 //     ($($x:expr),*) => (vec![$($x.to_string()),*]);
 // }
+// }}}
+// regex {{{
 lazy_static! {
     pub static ref REG: Regex = Regex::new(r#"(\d{1,}) (\w{1,}).*<(.*\.\w{1,})"#).unwrap();
 }
-
+// }}}
 fn main() {
+    // app {{{
+    let mut mailq = Vec::new();
     let matches = App::new("MyApp")
         .version("1.0")
         .author("ice")
@@ -42,54 +54,118 @@ fn main() {
                 .help("get ip"),
         )
         .subcommand(
+            SubCommand::with_name("a5")
+                .about("amout05")
+                .arg(Arg::with_name("emails").help("check email ips").index(1)),
+        )
+        .subcommand(
+            SubCommand::with_name("a6")
+                .about("amout06")
+                .arg(Arg::with_name("emails").help("check email ips").index(1)),
+        )
+        .subcommand(
             SubCommand::with_name("a7")
                 .about("amout07")
                 .arg(Arg::with_name("emails").help("check email ips").index(1)),
         )
+        .subcommand(
+            SubCommand::with_name("a8")
+                .about("amout08")
+                .arg(Arg::with_name("emails").help("check email ips").index(1)),
+        )
         .get_matches();
-
-    if let Some(matches) = matches.subcommand_matches("a7") {
-        println!("amout7 subcommand");
+    // }}}
+    // subcommands {{{
+    if let Some(matches) = matches.subcommand_matches("a5") {
+        // a5 ran
+        println!("amout5 subcommand");
         ssh(
+            &mut mailq,
             dotenv!("U3").to_string(),
             dotenv!("P2").to_string(),
             r"216.230.254.45".to_string(),
         );
+        // emails args supplied
+        // let mut full = &mailq;
+        // println!("full: {:?}", full);
         if let Some(i) = matches.value_of("emails") {
-            println!("Email args: {}", i);
-        }
-    }
-
-    if let Some(o) = matches.value_of("input") {
-        println!("INPUT: {}", o);
-        match o {
-            "ao5" => ssh(
+            println!("subcommand args: {}", i);
+            getip(
+                &mailq,
                 dotenv!("U3").to_string(),
                 dotenv!("P2").to_string(),
                 r"216.230.254.45".to_string(),
-            ),
-            "ao6" => ssh(
-                dotenv!("U3").to_string(),
-                dotenv!("P2").to_string(),
-                r"216.230.254.46".to_string(),
-            ),
-            "ao7" => ssh(
-                dotenv!("U3").to_string(),
-                dotenv!("P2").to_string(),
-                r"216.230.254.47".to_string(),
-            ),
-            "ao8" => ssh(
-                dotenv!("U3").to_string(),
-                dotenv!("P2").to_string(),
-                r"216.230.254.48".to_string(),
-            ),
+            );
+
+        }
+    }
+    // if let Some(matches) = matches.subcommand_matches("a6") {
+    //     println!("amout6 subcommand");
+    //     ssh(
+    //         dotenv!("U3").to_string(),
+    //         dotenv!("P2").to_string(),
+    //         r"216.230.254.46".to_string(),
+    //     );
+    //     if let Some(i) = matches.value_of("emails") {
+    //         println!("Email args: {}", i);
+    //     }
+    // }
+    // if let Some(matches) = matches.subcommand_matches("a7") {
+    //     println!("amout7 subcommand");
+    //     ssh(
+    //         dotenv!("U3").to_string(),
+    //         dotenv!("P2").to_string(),
+    //         r"216.230.254.47".to_string(),
+    //     );
+    //     if let Some(i) = matches.value_of("emails") {
+    //         println!("Email args: {}", i);
+    //     }
+    // }
+    // if let Some(matches) = matches.subcommand_matches("a8") {
+    //     println!("amout8 subcommand");
+    //     ssh(
+    //         dotenv!("U3").to_string(),
+    //         dotenv!("P2").to_string(),
+    //         r"216.230.254.4".to_string(),
+    //     );
+    //     if let Some(i) = matches.value_of("emails") {
+    //         println!("Email args: {}", i);
+    //     }
+    // }
+    // }}}
+    // match {{{
+    if let Some(o) = matches.value_of("input") {
+        println!("INPUT: {}", o);
+        match o {
+            // "ao5" => ssh(
+            //     dotenv!("U3").to_string(),
+            //     dotenv!("P2").to_string(),
+            //     r"216.230.254.45".to_string(),
+            // ),
+            // "ao6" => ssh(
+            //     dotenv!("U3").to_string(),
+            //     dotenv!("P2").to_string(),
+            //     r"216.230.254.46".to_string(),
+            // ),
+            // "ao7" => ssh(
+            //     dotenv!("U3").to_string(),
+            //     dotenv!("P2").to_string(),
+            //     r"216.230.254.47".to_string(),
+            // ),
+            // "ao8" => ssh(
+            //     dotenv!("U3").to_string(),
+            //     dotenv!("P2").to_string(),
+            //     r"216.230.254.48".to_string(),
+            // ),
             "ip" => ip(),
             "b" => build(),
             _ => println!("0"),
         }
     }
+    // }}}
 }
-
+// functions
+// build {{{
 fn build() {
     let utc: DateTime<Utc> = Utc::now();
     println!("{}", utc.to_string());
@@ -98,9 +174,9 @@ fn build() {
     let cmd = format!("cd {}/m/psp;cargo build", dotenv!("HOME"));
     cmd!("sh", "-c", cmd).run().unwrap();
 }
-
-fn ssh(user: String, pass: String, ip: String) {
-    let mut mailq = Vec::new();
+// }}}
+// ssh {{{
+fn ssh(mailq: &mut Vec<(MQ)>, user: String, pass: String, ip: String) {
     let command = format!("spawn ssh {}@{};expect \"password\";send \"{}\n\";expect \"root\";send \"mailq | grep \\$(date | awk \\'{{print \\$2}}\\') | sort -k 6 -r | sort -k 7 -r | uniq -c -f6 | sort -k 1 | awk \\'{{print \\$1,\\$2,\\$8}}\\'\\n\";expect \"root\";send \"exit\\n\";expect eof;exit", user, ip, pass);
     // println!("{}", command);
     let args = &["-c", command.as_str()];
@@ -113,15 +189,44 @@ fn ssh(user: String, pass: String, ip: String) {
         let email = cap[3].to_string();
         mailq.push((count, id, email));
     }
-    println!("{:?}", mailq);
-    println!("{:?}", mailq[0].1);
+    // println!("{:?}", mailq);
+    // println!("{:?}", mailq[0].1);
     // iterate vec, return vec
     // let q = mailq.iter().map(|x| &x.1).collect::<Vec<_>>();
     // println!("{:?}", q);
     // print elements of vec with iter
-    mailq.iter().map(|x| &x.1).for_each(|x| println!("{:?}", x));
-}
+    // mailq.iter().map(|x| &x.1).for_each(|x| println!("{:?}", x));
 
+    // sort mailq on count
+    let t2 = mailq;
+    t2.sort_by(|a, b| a.cmp(b));
+    // t2.sort_by_key(|x| &x.0);
+
+    // table output
+    let mut table = Table::new();
+    // header
+    table.add_row(row!["COUNT", "ID", "EMAILS"]);
+    // iterate mailq create new row
+    for x in t2 {
+        table.add_row(row![x.0, x.1, x.2]);
+    }
+    // table.add_row(row![mailq[0].0, mailq[0].1, mailq[0].2]);
+    // Print the table to stdout
+    table.printstd();
+}
+// }}}
+// getip {{{
+fn getip(mailq: &Vec<(MQ)>, user: String, pass: String, ip: String) {
+    // println!("{:?}{}{}{}", mailq, user, pass, ip);
+    // let command = format!("spawn ssh {}@{};expect \"password\";send \"{}\n\";expect \"root\";send \"echo -n \\'{}\\' && grep -Po \\'Received.*\\' /var/spool/mqueue/qf{} | tail -1 | grep -Po \\'\\\\\\[\\\\d{{1,3}}\\\\.\\\\d{{1,3}}\\\\.\\\\d{{1,3}}\\\\.\\\\d{{1,3}}\\\\\\]\\'\n\";expect \"root\";send \"exit\\n\";expect eof;exit", user, ip, pass, mailq[0].2, mailq[0].1);
+// tac /var/spool/mqueue/qfw5QAqD38270223 | sed -n '/From:/{p;n;p;n;p;n;p;n}'
+    let command = format!("spawn ssh {}@{};expect \"password\";send \"{}\n\";expect \"root\";send \"echo -n \\'{}\\' && grep -Po \\'Received.*\\' /var/spool/mqueue/qf{} | tail -1 | grep -Po \\'\\\\\\[\\\\d{{1,3}}\\\\.\\\\d{{1,3}}\\\\.\\\\d{{1,3}}\\\\.\\\\d{{1,3}}\\\\\\]\\'\n\";expect \"root\";send \"exit\\n\";expect eof;exit", user, ip, pass, mailq[0].2, mailq[0].1);
+    let args = &["-c", command.as_str()];
+    let stdout = cmd("expect", args).read().unwrap();
+    println!("{}", stdout);
+}
+// }}}
+// ip {{{
 fn ip() {
     let reader = maxminddb::Reader::open(
         "/home/fish/Downloads/GeoLite2-Country_20180605/GeoLite2-Country.mmdb",
@@ -130,7 +235,9 @@ fn ip() {
     let country: geoip2::Country = reader.lookup(ip).unwrap();
     print!("{:?}\n", country.country.unwrap().iso_code.unwrap());
 }
+// }}}
 
+// notes:
 // awk
 // https://stackoverflow.com/questions/1915636/is-there-a-way-to-uniq-by-column
 // https://unix.stackexchange.com/questions/204747/get-or-filter-duplicated-lines-by-column
@@ -148,8 +255,13 @@ fn ip() {
 // grep for ip
 // grep -Po '^H\?\?Received.*' /var/spool/mqueue/qfw5JFKFOG026685 | tail -1
 // FINAL GET IP:
-// grep -Po '^H\?\?Received.*' /var/spool/mqueue/qfw5Q1bTLI044854 | tail -1 | grep -Po '\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
+//echo -n 'yo' && grep -Po 'Received.*' /var/spool/mqueue/qfw5Q1bTLI044854 | tail -1 | grep -Po '\[(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\]'
+// grep -Po 'Received.*' /var/spool/mqueue/qfw5QAqD38270223 | tail -1 | grep -Po '\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
 // H??Received: from miycaaykdt (unknown [177.66.59.207])
+// get line matching brochure and print next 2 lines
+// sed -n '/Received:/{p;n;p;n;p;n;p;n}' /var/spool/mqueue/qfw5QAqD38270223
+// get line matching from and previous 3 lines
+// tac /var/spool/mqueue/qfw5QAqD38270223 | sed -n '/From:/{p;n;p;n;p;n;p;n}'
 
 // example output:
 // w5JEIDrg022952-    3369 Tue Jun 19 23:18 MAILER-DAEMON
@@ -164,3 +276,12 @@ fn ip() {
 // w5J7MCX9383350      802 Tue Jun 19 16:22 <info-satokon@sato-konpo.co.jp>
 // example stdout
 // [("1", "w5Q1bTLI044854", "hira90-24@ando-che.co.jp"), ("1", "w5Q1O0uW038769", "tokiya@sato-zen.co.jp"), ("1", "w5Q2f3mn074021", "o-i@funabori.co.jp"), ("1", "w5Q5gc5Y147098", "t.nawa@beams-dc.co.jp"), ("1", "w5Q6WE9o172191", "libro@jei.or.jp"), ("1", "w5Q6xtgH183057", "s-hayatsu@adrs-s.co.jp"), ("1", "w5Q6xuHF183075", "hazama-r@njr.jyutaku.co.jp"), ("1", "w5Q71g8k183924", "n.ishige@shantery.co.jp"), ("1", "w5Q77DNc186402", "master002@cds-ito.co.jp"), ("1", "w5Q7DVmV188873", "katsurada@keioizumi.co.jp"), ("1", "w5Q7EDUq189104", "fcshienbu@reins.co.jp"), ("1", "w5Q7EOBc189155", "m-furuse@kawamoto-ind.co.jp"), ("1", "w5Q7EQhj189180", "user@b0nds.jp"), ("1", "w5Q7ERVX189187", "teramoto@okouchi.co.jp"), ("2", "w5Q7EMla189140", "utm.m@nifty.com")]
+// example out
+// H??Received: from ampri02.alpha-mail.net (unknown [216.230.254.13])
+//         by amis06.alpha-mail.net (Postfix) with ESMTP;
+//         Tue, 26 Jun 2018 19:52:13 +0900 (JST)
+// H??Received: from oens05 (165-100-168-182.tokyo.otk.vectant.ne.jp [165.100.168.182])
+//         by ampri02.alpha-mail.net (Alpha-mail) with ESMTP id D4D5780040;
+//         Tue, 26 Jun 2018 19:52:12 +0900 (JST)
+// H??From: =?iso-2022-jp?B?GyRCPi5FRCEhNTFDSxsoQg==?= <oda@oens.co.jp>
+
